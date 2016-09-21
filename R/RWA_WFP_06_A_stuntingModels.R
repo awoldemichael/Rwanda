ed9ff23fec67fd351f6ee1b39d819b64a0bed6cc
Laura Hughes <laura.d.hughes@gmail.com>
2016-09-21 19:38:41 -0400

    remove NAs

--- R/RWA_WFP_06_A_stuntingModels.R
+++ R/RWA_WFP_06_A_stuntingModels.R
@@ -26,4 +26,4 @@ stunting_fits = ch %>% fit_with(lm, stunting_models)
 
 # basic fit ---------------------------------------------------------------
 
-summary(lm(formula = stuntingZ ~ wealth_idx + interview_date + FS_final + sex + age_months + FCS + impr_water + impr_toilet, data = ch))
+summary(lm(formula = stuntingZ ~ wealth_idx + interview_date + FS_final + sex + age_months + FCS + impr_water + impr_toilet, data = ch %>% filter(!is.na(isStunted))))
