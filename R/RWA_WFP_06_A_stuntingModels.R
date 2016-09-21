e536e3f2d5c60a75d57a8f6aefeb3b918d67e40b
Laura Hughes <laura.d.hughes@gmail.com>
2016-09-21 17:40:48 -0400

    init fits

---
+++ R/RWA_WFP_06_A_stuntingModels.R
@@ -0,0 +1,23 @@
+stunting_models = formulas(~stuntingZ, # lhs
+                           demo_child = 
+                             # -- demographic variables --
+                             ~age_months +
+                             sex,
+                           demo_child_interacted = ~age_months * sex,
+                             
+                           wealth = ~wealth_idx,
+                           both = add_predictors(demo_child_interacted, wealth))
+
+stunting_fits = ch %>% fit_with(lm, stunting_models)
+                           
+                             # # -- geo --
+                             # livelihood_zone + 
+                             # 
+                             # # -- WASH --
+                             # impr_unshared_toilet +
+                             # impr_water +
+                             # wash_knowl +
+                             # 
+                             # # -- nutrition --
+                             # ever_breastfed + 
+                             # FCS)
