84ea9b03fdaca7fbd6e5815f8f78b1e86e3cd3aa
flaneuse <laura.d.hughes@gmail.com>
2016-09-25 20:54:20 -0400

    switch dhs stats

--- R/RWA_WFP_P02_stuntingCHAIN.R
+++ R/RWA_WFP_P02_stuntingCHAIN.R
@@ -3,15 +3,17 @@
 
 # set constants -----------------------------------------------------------
 stunting_pal = rev(brewer.pal(11, 'Spectral')[1:6])
-stunting_range = c(0, 0.8)
+stunting_range = c(0, 0.80)
 
 IM_pal = c(grey15K, colorRampPalette(c('#66C2A5', '#1E4436'))(9)) # color ramp from Spectral #9 to Spectral #10 w/ grey15K appended to the start
 IM_range = c(0, 9)
 
-size_IM = 5
+size_stunting = 5
+size_IM = 3
+size_IM_label = 2.5
 
-squish_factor = 0.2
-x_init = 0.9
+squish_factor = 0.02
+x_init = 0.825
 
 # import CHAIN data -------------------------------------------------------
 
@@ -33,9 +35,10 @@ nutrition_tidy %>%
 # For rank-ordering:
 nutrit_order = nutrition_tidy %>% 
   group_by(shortName) %>% 
-  summarise(n = n()) %>% 
-  arrange(n) %>% 
-  mutate(x_posit = (row_number(n) - 1) * squish_factor + x_init) %>% 
+  summarise(n = n()) 
+
+nutrit_order = nutrit_order %>% 
+  mutate(x_posit = (nrow(nutrit_order) - row_number(n)) * squish_factor + x_init) %>% 
   select(-n) %>% 
   spread(shortName, x_posit)
 
@@ -61,84 +64,170 @@ chain = full_join(stunting_admin2,nutrition, by = c('admin2' = 'District'))
 
 ggplot(chain) +
   # -- error bars --
-  geom_segment(aes(x = lb_cfsva, xend = ub_cfsva, 
-                   y = fct_reorder(admin2, stunting_cfsva), 
-                   yend = fct_reorder(admin2, stunting_cfsva)), 
+  geom_segment(aes(x = lb_dhs, xend = ub_dhs, 
+                   y = fct_reorder(admin2, stunting_dhs), 
+                   yend = fct_reorder(admin2, stunting_dhs)), 
                colour = grey50K, alpha = 0.2,
                size = 1.25) +
+  
+  # -- country average --
+  geom_vline(xintercept = 0.38,
+             size = 0.5, colour = grey70K) +
+  
   # -- averages (squares) --
-  geom_point(aes(x = stunting_cfsva, 
-                 y = fct_reorder(admin2, stunting_cfsva), 
-                 fill = stunting_cfsva),
-             size = 4, shape = 22, colour = grey90K) +
+  geom_point(aes(x = stunting_dhs, 
+                 y = fct_reorder(admin2, stunting_dhs), 
+                 fill = stunting_dhs),
+             size = size_stunting, shape = 22, colour = grey90K) +
   
   # -- overlay %s on top --
-  geom_text(aes(x = stunting_cfsva, 
-                y = fct_reorder(admin2, stunting_cfsva), 
-                label = round(stunting_cfsva * 100, 0)),
+  geom_text(aes(x = stunting_dhs, 
+                y = fct_reorder(admin2, stunting_dhs), 
+                label = round(stunting_dhs * 100, 0)),
             size = 2, colour = grey90K) +
   
   # -- binary values for CHAIN projects --
   geom_point(aes(x = nutrit_order$AEE, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = AEE),
              size = size_IM) +
   geom_point(aes(x = nutrit_order$Caritas, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = Caritas),
              size = size_IM) +
   geom_point(aes(x = nutrit_order$FXB, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = FXB),
              size = size_IM) +
   geom_point(aes(x = nutrit_order$HarvestPlus, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = HarvestPlus),
              size = size_IM) +
   geom_point(aes(x = nutrit_order$IILP, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = IILP),
              size = size_IM) +
   geom_point(aes(x = nutrit_order$INWA, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = INWA),
              size = size_IM) +
   geom_point(aes(x = nutrit_order$ISVP, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = ISVP),
              size = size_IM) +
   geom_point(aes(x = nutrit_order$OFSP, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = OFSP),
              size = size_IM) +
   geom_point(aes(x = nutrit_order$PSDAG, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = PSDAG),
              size = size_IM) +
   geom_point(aes(x = nutrit_order$RRSA, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = RRSA),
              size = size_IM) +
   geom_point(aes(x = nutrit_order$RSMP, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = RSMP),
              size = size_IM) +
   geom_point(aes(x = nutrit_order$`RDCP II`, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = `RDCP II`),
              size = size_IM) +
   geom_point(aes(x = nutrit_order$`ROADS III`, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = `ROADS III`),
              size = size_IM) +
+  
+  # -- Text labels --
+  geom_text(aes(x = nutrit_order$AEE, 
+                 y = 0,
+                 label = 'AEE'),
+             size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  geom_text(aes(x = nutrit_order$Caritas, 
+                y = 0,
+                label = 'Caritas'),
+            size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  geom_text(aes(x = nutrit_order$FXB, 
+                y = 0,
+                label = 'FXB'),
+            size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  geom_text(aes(x = nutrit_order$HarvestPlus, 
+                y = 0,
+                label = 'HarvestPlus'),
+            size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  geom_text(aes(x = nutrit_order$IILP, 
+                y = 0,
+                label = 'IILP'),
+            size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  geom_text(aes(x = nutrit_order$INWA, 
+                y = 0,
+                label = 'INWA'),
+            size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  geom_text(aes(x = nutrit_order$ISVP, 
+                y = 0,
+                label = 'ISVP'),
+            size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  geom_text(aes(x = nutrit_order$OFSP, 
+                y = 0,
+                label = 'OFSP'),
+            size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  geom_text(aes(x = nutrit_order$PSDAG, 
+                y = 0,
+                label = 'PSDAG'),
+            size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  geom_text(aes(x = nutrit_order$`RDCP II`, 
+                y = 0,
+                label = 'RDCP'),
+            size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  geom_text(aes(x = nutrit_order$`ROADS III`, 
+                y = 0,
+                label = 'ROADS'),
+            size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  geom_text(aes(x = nutrit_order$RRSA, 
+                y = 0,
+                label = 'RRSA'),
+            size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  geom_text(aes(x = nutrit_order$RSMP, 
+                y = 0,
+                label = 'RSMP'),
+            size = size_IM_label,
+            family = font_light,
+            colour = grey70K) +
+  
 
   # -- totals --
-    geom_point(aes(x = max(nutrit_order) + squish_factor, 
-                 y = fct_reorder(admin2, stunting_cfsva),
+    geom_point(aes(x = max(nutrit_order) + 2 * squish_factor, 
+                 y = fct_reorder(admin2, stunting_dhs),
                  colour = total),
-             size = size_IM) +
-  geom_text(aes(x = max(nutrit_order) + squish_factor, 
-                y = fct_reorder(admin2, stunting_cfsva), 
+             size = size_stunting) +
+  geom_text(aes(x = max(nutrit_order) + 2 * squish_factor, 
+                y = fct_reorder(admin2, stunting_dhs), 
                 label = total),
             size = 3, colour = grey10K) +
   
@@ -149,7 +238,8 @@ ggplot(chain) +
   scale_fill_gradientn(colours = stunting_pal, limits = stunting_range) +
   scale_x_continuous(name = 'percent of stunted children (under 5 years old)',
                      labels = scales::percent,
-                     breaks = c(0, 0.2, 0.4, 0.6, 0.8)) +
+                     breaks = c(0, 0.25, 0.50, 0.75)) +
+                     # breaks = c(0, 0.2, 0.4, 0.6, 0.8)) +
   # -- themes --
   theme_xygridlight() +
   theme(axis.title.y = element_blank(), 
@@ -158,5 +248,6 @@ ggplot(chain) +
   )
 
 
-# save_plot('~/Creative Cloud Files/MAV/Projects/RWA_LAM-stunting_2016-09/exported_fromR/RWA_02_CHAIN_stunting.pdf')
+save_plot('~/Creative Cloud Files/MAV/Projects/RWA_LAM-stunting_2016-09/exported_fromR/RWA_02_CHAIN_stunting_dhs.pdf',
+          width = 10, height = 7)
 # 
