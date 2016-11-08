78eac7038f93052b1e5614ba986a3697d284de98
flaneuse <laura.d.hughes@gmail.com>
2016-11-08 11:17:22 -0500

    clean fluff

--- R/RWA_WFP_04_importKids2012.R
+++ R/RWA_WFP_04_importKids2012.R
@@ -35,39 +35,6 @@ ch2012_raw = read_sav(paste0(baseDir, 'RW_2012_CFSVA/cfsvans-2012- children-v01.
 
 # Checking how weights should be applied ----------------------------------
 
-# According to CFSVA final report, they used a two-stage survey design, with the districts as the primary cut and villages as the secondary:
-# (from CFSVA 2015 detailed survey methodolgy)
-# To facilitate comparison with existing studies, the CFSVA 2015 was designed to provide statistically representative
-# and precise information at the district level. In addition, it was decided to include both urban and rural households
-# and not to exclude the capital province Kigali. The sampling frame was organized according to the 30 districts.
-# Subsequently, a two-stage cluster sample procedure was applied.
-# In the first stage, 25 villages per district were randomly selected with probability to be selected proportional to the
-# population size. In the second stage, ten households in each of the 25 villages in the 30 provinces were selected for
-# participation in the survey. A systematic random sampling technique was chosen for this stage. The team leader,
-# together with the village head, listed all households in the village. Based on this list, a systematic random sample
-# was utilized to pick ten households to be interviewed and three reserve households should any of the first ten
-# households be missing at the time of the interview or not agree to participate. Households were eligible for
-# participation in the assessment if living in one of the selected villages at the time of the interviews.
-# Thus, ten households, from 25 villages, from 30 provinces were chosen to participate in the survey, amounting up
-# to 7,500 households.
-
-# And from the NISR explanation of the data:
-# "Taking into consideration the two-stage cluster sampling methodology described above, adjustment weights were
-# computed to provide results representative at country level. The household probability of being selected in the
-# sample is equal to the product of a household’s probability of being selected in a village by the probability of the
-# village of being sampled. The inverse of this probability is the design weight. The design weight was adjusted for
-# the expected and actual number of households in the surveyed villages and was used in the complex sample
-# calculations. The design weight was divided by the product of the total number of households in the population
-# divided by the number of sampled households. The resulting weight was used in all non-complex sample analyses."
-
-# Based on this info, it seems like the primary strata = 30 districts (S0_D_Dist) and the enumeration areas are the villages (S0_G_Vill)
-
-
-# Seems right on target w/ the strata being the 30 districts (first sampling division)
-# Comparing final numbers from the CFSVA to the ones I calculated
-# Point of difference: report says there are 4058 children measured, but the smaple only contains 3810.
-# Guessing (?) difference is that there were 4058 eligible, but only 3810 measured / valid. 
-# Raw children's file very clearly has 280 NAs, and numbers check out.
 
 
 # Notes on data -----------------------------------------------------------
