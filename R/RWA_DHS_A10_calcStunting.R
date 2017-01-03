45a5f93d74cfc3a4929529c53771c99cc432eb9b
flaneuse <laura.d.hughes@gmail.com>
2017-01-03 12:10:38 -0500

    testing beeswarm

--- R/RWA_DHS_A10_calcStunting.R
+++ R/RWA_DHS_A10_calcStunting.R
@@ -16,3 +16,36 @@ write.csv(st_dhs, '~/Github/Rwanda/exported_data/DHS_stunted_unweighted.csv')
 st_dhs = calcPtEst(df2, 'stunted', by_var = 'lvdzone2', use_weights = TRUE,
                    psu_var = 'psu', strata_var = 'strata', weight_var = 'cweight')
 write.csv(st_dhs, '~/Github/Rwanda/exported_data/DHS_stunted_weighted.csv')
+
+
+library(ggbeeswarm)
+
+dhs = removeAttributes(df) %>% 
+  filter(!is.na(stunting))
+
+
+
+dhs = factorize(dhs, df, 'lvdzone', 'lvdzone')
+
+lz_order = dhs %>% 
+  filter(year == 2014) %>% 
+  group_by(lvdzone) %>% 
+  summarise(avg = mean(stunting, na.rm = TRUE)) %>% 
+  arrange(desc(avg))
+
+dhs$lvdzone = factor(dhs$lvdzone, levels = lz_order$lvdzone)
+
+ggplot(dhs, aes(y = stunting, x = lvdzone, 
+                group = lvdzone,
+                colour = factor(year), fill = year)) + 
+
+  geom_quasirandom(alpha = 0.6) +
+  # geom_violin(alpha = 0.4, fill = NA)+
+  # geom_boxplot(fill = NA, colour = grey75K, size = 0.2) +
+  geom_hline(yintercept = -2) +
+  
+    # facet_wrap(~year) +
+  coord_flip() +
+  scale_color_manual(values = c('2010' = grey60K, '2014' = brewer.pal(11, 'Spectral')[1])) +
+   # scale_colour_gradientn(colours = brewer.pal(11, 'Spectral')[1:6]) +
+  theme_xgrid()
