24cf17b6985a42a1a6a9192632f38637b0723687
flaneuse <laura.d.hughes@gmail.com>
2017-01-19 14:22:33 -0500

    init

---
+++ R/RWA_WFP_A09_stuntingAvgs2012-2015.R
@@ -0,0 +1,23 @@
+# Rwanda stunting analysis -----------------------------------------
+#
+# RWA_WFP_A09_stuntingAvgs2012-2015.R: calculate stunting averages for DHS, CFSVS for 2010-2015 data
+#
+# Script to pull stunting data and associated household- or
+# child-level data for Rwanda from the CFSVA dataset
+# 
+# Data are from the 2015 Comprehensive Food Security and Vulnerability Analysis
+# by the World Food Programme
+# Available at http://microdata.statistics.gov.rw/index.php/catalog/70
+# Report: https://www.wfp.org/content/rwanda-comprehensive-food-security-and-vulnerability-analysis-march-2016
+#
+# Laura Hughes, lhughes@usaid.gov, 14 September 2016
+# with Tim Essam (tessam@usaid.gov) and Nada Petrovic (npetrovic@usaid.gov)
+#
+# Copyright 2016 by Laura Hughes via MIT License
+#
+# -------------------------------------------------------------------------
+
+setwd('~/GitHub/Rwanda/R/')
+  
+
+# SETUP -------------------------------------------------------------------
