d4cdd573cf8e8b3a7774404a566cc036b8833b02
flaneuse <laura.d.hughes@gmail.com>
2017-04-06 16:30:03 -0400

    food security tables

---
+++ R/RWA_WFP_A12_calcFoodSec.R
@@ -0,0 +1,54 @@
+
+# setup -------------------------------------------------------------------
+setwd('~/GitHub/Rwanda/R/')
+exportDir = '~/Documents/USAID/Rwanda/processeddata/'  
+
+
+# source DHS stats --------------------------------------------------------
+
+
+# source CFSVA data -------------------------------------------------------
+source('RWA_WFP_run2015.R')
+
+
+food_access_lz = hh %>% 
+  group_by(livelihood_zone, food_access_year_cat) %>% 
+  summarise(foodAccess_N = n()) %>% 
+  ungroup() %>% 
+  group_by(livelihood_zone) %>% 
+  mutate(foodAccess_pct = foodAccess_N / sum(foodAccess_N))
+
+
+food_access_dist = hh %>% 
+  group_by(admin2, food_access_year_cat) %>% 
+  summarise(foodAccess_N = n()) %>% 
+  ungroup() %>% 
+  group_by(admin2) %>% 
+  mutate(foodAccess_pct = foodAccess_N / sum(foodAccess_N))
+
+
+
+cari_lz = hh %>% 
+  group_by(livelihood_zone, CARI_cat) %>% 
+  summarise(CARI_N = n()) %>% 
+  ungroup() %>% 
+  group_by(livelihood_zone) %>% 
+  mutate(CARI_pct = CARI_N / sum(CARI_N))
+
+
+cari_dist = hh %>% 
+  group_by(admin2, CARI_cat) %>% 
+  summarise(CARI_N = n()) %>% 
+  ungroup() %>% 
+  group_by(admin2) %>% 
+  mutate(CARI_pct = CARI_N / sum(CARI_N))
+
+dist = full_join(cari_dist, food_access_dist)
+lz = full_join(cari_lz, food_access_lz)
+
+
+
+# export ------------------------------------------------------------------
+
+write.csv(dist, paste0(exportDir, 'RWA_CFSVA_2015_foodAccess_district.csv'))
+write.csv(lz, paste0(exportDir, 'RWA_CFSVA_2015_foodAccess_lz.csv'))
